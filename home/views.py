74fa746aed4ee59fd1ccabe7d86911707d1d0597
serSalas <66839038+serSalas@users.noreply.github.com>
2022-11-03 01:22:54 -0300

    modify MVT to add home app

--- home/views.py
+++ home/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from datetime import datetime
+# Create your views here.
 
 def home(request):
-    return HttpResponse('Hello, World')
+    return render(request, 'home/welcome.html', {'today':datetime.today()})
