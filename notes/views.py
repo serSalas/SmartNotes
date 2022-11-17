b719f77d1593210dad76c28d066d9ed0490878b9
serSalas <66839038+serSalas@users.noreply.github.com>
2022-11-17 20:30:55 -0300

    fix error

--- notes/views.py
+++ notes/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 from .models import Notes
+
+def list(request):
+    all_notes = Notes.objects.all()
+    retrun render(request, 'notes/notes_list.html', {'notes' : all_notes})
