641833211456dbcbca25eac60eddab5ab859ff95
serSalas <66839038+serSalas@users.noreply.github.com>
2022-11-19 14:21:20 -0300

    to change authorized function to authorized class

--- home/views.py
+++ home/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from datetime import datetime
-from django.contrib.auth.decorators import login_required
 from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 class HomeView(TemplateView):
     template_name = 'home/welcome.html'
     extra_context = {'today': datetime.today()}
 
-@login_required(login_url='/admin')
-def authorized(request):
-    return render(request, 'home/authorized.html',{})
+class AuthorizedView(TemplateView):
+    template_name = 'home/authorized.html'
+    login_url = '/admin'
