395c4f2d9609a752b083073a94279bdd26c58666
serSalas <66839038+serSalas@users.noreply.github.com>
2022-11-19 14:18:20 -0300

    change function home view to class home view

--- home/views.py
+++ home/views.py
@@ -2,11 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from datetime import datetime
 from django.contrib.auth.decorators import login_required
+from django.views.generic import TemplateView
 
-# Create your views here.
-
-def home(request):
-    return render(request, 'home/welcome.html', {'today':datetime.today()})
+class HomeView(TemplateView):
+    template_name = 'home/welcome.html'
+    extra_context = {'today': datetime.today()}
 
 @login_required(login_url='/admin')
 def authorized(request):
